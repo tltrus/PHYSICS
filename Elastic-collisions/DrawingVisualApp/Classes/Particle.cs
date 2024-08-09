d401351b8a27a4e322c77ce547fe0216f5826adb
tltrus <77125487+tltrus@users.noreply.github.com>
2024-08-09 19:47:57 +0400

    up

---
+++ Elastic-collisions/DrawingVisualApp/Classes/Particle.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace DrawingVisualApp
+{
+    class Particle
+    {
+        public Vector2D position, velocity, acceleration;
+        public int mass, id;
+        public double r;
+        public Brush brush;
+
+        public Particle(double x, double y, int mass, int id, Brush brush)
+        {
+            position = new Vector2D(x, y);
+            acceleration = new Vector2D(0, 0);
+            velocity = Vector2D.Random2D();
+            velocity.Mult(MainWindow.rnd.NextDouble());
+            this.brush = brush;
+            this.mass = mass;
+            this.id = id;
+            this.r = Math.Sqrt(mass) * 2;
+        }
+
+        public void ApplyForce(Vector2D force)
+        {
+            var f = force.CopyToVector();
+            f.Div(mass);
+            acceleration.Add(f);
+        }
+        public void Update()
+        {
+            velocity.Add(acceleration);
+            position.Add(velocity);
+            acceleration.Mult(0);
+        }
+        public void Collide(Particle other)
+        {
+            var impactVector = Vector2D.Sub(other.position, position);
+            var d = impactVector.Mag();
+            if (d < r + other.r)
+            {
+                // Push the particles out so that they are not overlapping
+                var overlap = d - (r + other.r);
+                var dir = impactVector.CopyToVector();
+                dir.SetMag(overlap * 0.5);
+                position.Add(dir);
+                other.position.Sub(dir);
+
+                // Correct the distance!
+                d = r + other.r;
+                impactVector.SetMag(d);
+
+                var mSum = mass + other.mass;
+                var vDiff = Vector2D.Sub(other.velocity, velocity);
+                // Particle A (this)
+                var num = vDiff.Dot(impactVector);
+                var den = mSum * d * d;
+                var deltaVA = impactVector.CopyToVector();
+                deltaVA.Mult((2 * other.mass * num) / den);
+                velocity.Add(deltaVA);
+                // Particle B (other)
+                var deltaVB = impactVector.CopyToVector();
+                deltaVB.Mult((-2 * mass * num) / den);
+                other.velocity.Add(deltaVB);
+            }
+        }
+        public void Edges()
+        {
+            if (position.X > MainWindow.width - r)
+            {
+                position.X = MainWindow.width - r;
+                velocity.X *= -1;
+            }
+            else if (position.X < r)
+            {
+                position.X = r;
+                velocity.X *= -1;
+            }
+
+            if (position.Y > MainWindow.height - r)
+            {
+                position.Y = MainWindow.height - r;
+                velocity.Y *= -1;
+            }
+            else if (position.Y < r)
+            {
+                position.Y = r;
+                velocity.Y *= -1;
+            }
+        }
+        public void Show(DrawingContext dc)
+        {
+            // Circle
+            dc.DrawEllipse(brush, null, new Point(position.X, position.Y), r, r);
+        }
+
+        public void ShowVector(DrawingContext dc)
+        {
+            // Vector of direction
+            var v = velocity.CopyToVector();
+            v.SetMag(r);
+            var pv = position + v;
+            Point p0 = new Point(position.X, position.Y);
+            Point p1 = new Point(pv.X, pv.Y);
+            dc.DrawLine(new Pen(Brushes.LightGray, 1), p0, p1);
+        }
+    }
+}
---
+++ Elastic-collisions/DrawingVisualApp/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+
+namespace DrawingVisualApp
+{
+    // Based on #184 — Elastic Collisions https://thecodingtrain.com/challenges/184-elastic-collisions
+    public partial class MainWindow : Window
+    {
+        System.Windows.Threading.DispatcherTimer timer;
+        public static Random rnd = new Random();
+        public static int width, height;
+
+        DrawingVisual visual;
+        DrawingContext dc;
+        List<Particle> particles = new List<Particle>();
+
+        Brush[] brushes = new Brush[] { Brushes.Brown, Brushes.Red, Brushes.Green, Brushes.Yellow, Brushes.White, Brushes.Blue, Brushes.Pink };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            visual = new DrawingVisual();
+
+            width = (int)g.Width;
+            height = (int)g.Height;
+
+            timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 20);
+
+            Init();
+        }
+
+        private void Init()
+        {
+            for (int i = 0; i < 50; ++i)
+            {
+                var x = rnd.Next(width);
+                var y = rnd.Next(height);
+                var brush = brushes[rnd.Next(brushes.Count())];
+                var mass = rnd.Next(4, 260);
+                var particle = new Particle(x, y, mass, i, brush);
+
+                particles.Add(particle);
+            }
+            timer.Start();
+        }
+
+
+        private void timerTick(object sender, EventArgs e) => Drawing();
+
+        private void Drawing()
+        {
+            g.RemoveVisual(visual);
+            using (dc = visual.RenderOpen())
+            {
+                var boundary = new Rectangle(0, 0, width, height);
+                var qtree = new QuadTree(boundary, 4);
+
+                foreach (var p in particles)
+                {
+                    var point = new myPoint(p.position.X, p.position.Y, p);
+                    qtree.Insert(point);
+
+                    p.Update();
+                    p.Edges();
+                    p.Show(dc);
+
+                    if (cbShowVectors.IsChecked == true)
+                        p.ShowVector(dc);
+                }
+
+                foreach (var p in particles)
+                {
+                    var range = new Circle(p.position.X, p.position.Y, (int)p.r * 2);
+                    var points = qtree.Query(range);
+                    foreach (var point in points)
+                    {
+                        var other = point.userData;
+                        if (p != other)
+                        {
+                            p.Collide(other);
+                        }
+                    }
+                }
+
+                if (cbShowQtree.IsChecked == true)
+                    qtree.Show(dc);
+
+                dc.Close();
+                g.AddVisual(visual);
+            }
+        }
+
+        private void g_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
